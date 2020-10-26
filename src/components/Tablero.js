10d30f777951dfc56dcc9790cf5d171be1872b59
Joshua Belden <github@joshuabelden.com>
2020-10-26 14:58:30 -0700

    Updated layout.

--- src/components/Tablero.js
+++ src/components/Tablero.js
@@ -4,26 +4,46 @@ import { obtenerVerbos } from '../actions/verbos';
 
 const ComponenteVerbo = (params) => {
     if (!params.verboSeleccionado) {
-        return <Fragment></Fragment>
+        return <Fragment></Fragment>;
     }
 
     console.log('params', params);
 
-    switch (params.formaSeleccionada) {
-        case 'yo':
-            return <Fragment>{params.verboSeleccionado.form_1s}</Fragment>;
-        case 'tu':
-            return <Fragment>{params.verboSeleccionado.form_2s}</Fragment>;
-        case "usted":
-            return <Fragment>{params.verboSeleccionado.form_3s}</Fragment>;
-        case "nosotros":
-            return <Fragment>{params.verboSeleccionado.form_1p}</Fragment>;
-        case "ustedes":
-            return <Fragment>{params.verboSeleccionado.form_3p}</Fragment>;
-        default: 
-            return <Fragment></Fragment>;
+    return <Fragment>
+        <h2>{obtenerFormaDelVerbo(params.formaSeleccionada, params.verboSeleccionado)}</h2>
+    </Fragment>;
+};
+
+const ComponenteDetallesDelVerbo = (params) => {
+    if (!params.mostrarDetalles || !params.verboSeleccionado) {
+        return <Fragment></Fragment>;
     }
-}
+    return <Fragment>
+        <h3>{params.verboSeleccionado.infinitive}</h3>
+        <p>
+            Presente Indicativo para {params.formaSeleccionada}
+        </p>
+        <p>{params.verboSeleccionado.infinitive_english}</p>
+        <p>{params.verboSeleccionado.verb_english}</p>
+    </Fragment>;
+};
+
+const obtenerFormaDelVerbo = (forma, verbo) => {
+    switch (forma) {
+        case 'yo':
+            return verbo.form_1s;
+        case 'tu':
+            return verbo.form_2s;
+        case "usted":
+            return verbo.form_3s;
+        case "nosotros":
+            return verbo.form_1p;
+        case "ustedes":
+            return verbo.form_3p;
+        default:
+            return "";
+    }
+};
 
 const Tablero = ({
     verbos,
@@ -31,27 +51,29 @@ const Tablero = ({
 }) => {
 
     useEffect(() => {
-		obtenerVerbos();
+        obtenerVerbos();
     }, [obtenerVerbos]);
-    
+
     const [formaSeleccionada, estabFormaSeleccionada] = useState();
     const [verboSeleccionado, estabVerboSeleccionado] = useState();
+    const [mostrarDetalles, estabMostrarDetalles] = useState();
 
     const handleUsarPresenteCambiar = (checked) => {
         estabUsarPresente(checked);
-    }
+    };
 
     const [usarPresente, estabUsarPresente] = useState(true);
 
-    function next() {
+    function siguiente() {
         estabFormaSeleccionada(obtenerFormaSelecionada());
         estabVerboSeleccionado(obtenerVerboAleatoria());
+        estabMostrarDetalles(true);
     }
 
     function obtenerFormaSelecionada() {
-        return ['yo','tu','usted','nosotros', 'ustedes'][obtenerAleatoria(5)];
+        return ['yo', 'tu', 'usted', 'nosotros', 'ustedes'][obtenerAleatoria(5)];
     }
-    
+
     function obtenerVerboAleatoria() {
         const available = verbos.filter(v => {
             return usarPresente && v.mood === 'Indicativo' && v.tense === 'Presente';
@@ -64,17 +86,29 @@ const Tablero = ({
         return Math.floor((Math.random() * máx) + 0);
     }
 
+    function mostrarDetallesDelVerbo() {
+        estabMostrarDetalles(true);
+    }
+
     return (
         <Fragment>
             <div className="page-wrapper">
                 <header id="header" className="alt">
-                    <input type="checkbox" checked={usarPresente} onChange={(e) => handleUsarPresenteCambiar(e.target.checked)} value="presente" />Presente
+                    <input type="checkbox" checked={usarPresente} onChange={(e) => handleUsarPresenteCambiar(e.target.checked)} value="presente indicativo" />Presente
                 </header>
                 <section id="main" className="container">
-                    <ComponenteVerbo formaSeleccionada={formaSeleccionada} verboSeleccionado={verboSeleccionado}></ComponenteVerbo>
+                    <div>
+                        <button onClick={siguiente}>Siguiente Verbo</button>
+                        <button onClick={(e) => mostrarDetallesDelVerbo()}>Mostrar Detalles</button>
+                    </div>
+                    <div>
+                        <ComponenteVerbo formaSeleccionada={formaSeleccionada} verboSeleccionado={verboSeleccionado}></ComponenteVerbo>
+                    </div>
+                    <div>
+                        <ComponenteDetallesDelVerbo formaSeleccionada={formaSeleccionada} verboSeleccionado={verboSeleccionado} mostrarDetalles={mostrarDetalles}></ComponenteDetallesDelVerbo>
+                    </div>
                 </section>
                 <footer>
-                    <button onClick={next}>Next</button>
                 </footer>
             </div>
 
